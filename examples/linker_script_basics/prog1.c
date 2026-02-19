a063cb5afa1d466edc198073aadaf385de96ec17
Shubham Singh <shubham.ece@outlook.com>
2026-02-19 08:46:15 +0000

    examples: added new examples: 	1: basics 	2: linker_script_basics
    Signed-off-by: Shubham Singh <shubham.ece@outlook.com>

---
+++ examples/linker_script_basics/prog1.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2026 embeddedplayground.com. All rights reserved.
+ * Author: Shubham <shubham@nexusrtos.com>
+ *
+ */
+
+int global_prog1;
+
+int leftshift_by_two(int x)
+{
+	x = x<<2;
+	return x; 
+}
---
+++ examples/linker_script_basics/prog2.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2026 embeddedplayground.com. All rights reserved.
+ * Author: Shubham <shubham@nexusrtos.com>
+ *
+ */
+
+
+int global_prog2 = 10;
+
+int rightshift_by_two(int x)
+{
+	x = x>>2;
+	return x;
+}
